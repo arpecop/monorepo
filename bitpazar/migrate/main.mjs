2ae6ecdbaac89d4cda5abe06e4690844e2f1e2bd
Your Name <you@example.com>
2025-08-03 09:52:28 +0000

    c

--- bitpazar/migrate/main.mjs
+++ bitpazar/migrate/main.mjs
@@ -1,5 +1,5 @@
-import pg from "pg";
 import nano from "nano";
+import postgres from "postgres";
 
 import { QdrantClient } from "@qdrant/js-client-rest";
 
@@ -7,72 +7,70 @@ import { QdrantClient } from "@qdrant/js-client-rest";
 const PG_HOST = "192.168.100.102";
 const PG_USER = "rudix";
 const PG_PASSWORD = "maximus";
-const PG_DATABASE = "rudix";
-const PG_PORT = 5432;
+
 const LIMIT = 500;
 
-const PG_CONNECTION_STRING = `postgres://${PG_USER}:${PG_PASSWORD}@${PG_HOST}:${PG_PORT}/${PG_DATABASE}`;
 const COUCHDB_URL = `http://${PG_USER}:${PG_PASSWORD}@${PG_HOST}:5984`;
 
-// Milvus Configuration
+const sql = postgres("postgres://rudix:maximus@bee.local:5432/rudix");
 
 const client = new QdrantClient({ host: PG_HOST, port: 6333 });
-// wait until connecting finished
-//
 
-const pgClient = new pg.Client(PG_CONNECTION_STRING);
-await pgClient.connect();
 const couch = nano(COUCHDB_URL);
+let couchDb = null;
 
-const runMigration = async (TABLE, COUCHDB_DB_NAME) => {
-  console.log("Starting data migration orchestrator...");
+const runMigration = async (table, couchname) => {
+  const fullTable = `qa.${table}`;
+  const query = await sql`
+  SELECT *
+  FROM qa.ai  
+  WHERE processed IS NULL
+  LIMIT ${LIMIT}
+`;
+  console.log("qd", couchname);
+  if (!couchDb) {
+    couchDb = couch.use(couchname);
+  }
 
-  console.log("Connected to PostgreSQL.");
-
-  const couchDb = couch.use(COUCHDB_DB_NAME);
-
-  const query = await pgClient.query(
-    `select * from ${TABLE} where processed is null limit  ${LIMIT}`,
-  );
-  const rows = query.rows.filter((x) => x.embed && x.embed.length > 0);
-  if (query.rows.length > 10) {
+  const rows = query.filter((x) => x.embed && x.embed.length > 0);
+  if (query.length > 10) {
     const couchx = Promise.all(
-      query.rows.map((document) =>
+      rows.map((document) =>
         couchDb
           .insert({
             _id:
-              document.genid.length > 2
+              document.genid && document.genid.length > 2
                 ? document.genid
                 : document.id.toString(),
             ...document,
             enbed: JSON.parse(document.embed),
+            type: couchname,
           })
           .catch((e) => null),
       ),
     );
 
-    const vecs = client.upsert(COUCHDB_DB_NAME, {
+    const vecs = client.upsert(couchname, {
       wait: true,
       points: rows.map((x) => ({
         id: x.id,
-        payload: x,
+        payload: { ...x, type: couchname },
         vector: JSON.parse(x.embed),
       })),
     });
     const updates = Promise.all(
-      query.rows.map((row) =>
-        pgClient.query(
-          `UPDATE ${TABLE} SET "processed"='1' WHERE "id" = ${row.id}`,
-        ),
+      query.map(
+        (row) => sql`UPDATE qa.ai SET "processed"='1' WHERE "id" = ${row.id}`,
       ),
     );
-    await Promise.all([vecs, updates, couchx]);
-    console.log(query.rows[0].id, LIMIT, query.rows.length);
-    runMigration(TABLE, COUCHDB_DB_NAME);
+
+    await Promise.all([vecs, couchx, updates]);
+    console.log(query[0].id, couchname, query.length);
+    runMigration(table, couchname);
   } else {
     console.log("DONE");
     throw new Error();
   }
 };
 //
-runMigration("qa.ai", "ai");
+runMigration(`qa.ai`, "ai");
--- bitpazar/migrate/tags.mjs
+++ bitpazar/migrate/tags.mjs
@@ -1,103 +1,76 @@
-import pg from "pg";
 import nano from "nano";
+import postgres from "postgres";
 
-import { MilvusClient, DataType } from "@zilliz/milvus2-sdk-node";
+import { QdrantClient } from "@qdrant/js-client-rest";
 
 // Configuration
-const TABLE = "qa.cats";
 const PG_HOST = "192.168.100.102";
 const PG_USER = "rudix";
 const PG_PASSWORD = "maximus";
-const PG_DATABASE = "rudix";
-const PG_PORT = 5432;
-const LIMIT = 2500;
 
-const PG_CONNECTION_STRING = `postgres://${PG_USER}:${PG_PASSWORD}@${PG_HOST}:${PG_PORT}/${PG_DATABASE}`;
+const LIMIT = 500;
+
 const COUCHDB_URL = `http://${PG_USER}:${PG_PASSWORD}@${PG_HOST}:5984`;
-const COUCHDB_DB_NAME = "cats";
 
-// Milvus Configuration
+const sql = postgres("postgres://rudix:maximus@bee.local:5432/rudix");
 
-const clientx = new MilvusClient({
-  address: PG_HOST + ":19530",
-  token: "root:Milvus",
-});
-// wait until connecting finished
-await clientx.connectPromise;
+const client = new QdrantClient({ host: PG_HOST, port: 6333 });
 
-const fields = [
-  {
-    name: "id",
-    data_type: DataType.Int64,
-    is_primary_key: true,
-    auto_id: false,
-  },
-  {
-    name: "embed",
-    data_type: DataType.FloatVector,
-    dim: 32,
-  },
-  {
-    name: "genid",
-    data_type: DataType.VarChar,
-    max_length: 512,
-  },
-];
+const couch = nano(COUCHDB_URL);
+let couchDb = null;
 
-await clientx.createCollection({
-  collection_name: COUCHDB_DB_NAME,
-  fields,
-});
-const pgClient = new pg.Client(PG_CONNECTION_STRING);
+const runMigration = async (table, couchname) => {
+  const fullTable = `qa.${table}`;
+  const query = await sql`
+  SELECT *
+  FROM qa.tags 
+  WHERE processed IS NULL
+  LIMIT ${LIMIT}
+`;
+  console.log("qd", couchname);
+  if (!couchDb) {
+    couchDb = couch.use(couchname);
+  }
 
-await pgClient.connect();
-
-const runMigration = async () => {
-  console.log("Starting data migration orchestrator...");
-
-  console.log("Connected to PostgreSQL.");
-
-  const couch = nano(COUCHDB_URL);
-  const couchDb = couch.use(COUCHDB_DB_NAME);
-
-  const query = await pgClient.query(
-    `select * from ${TABLE} where processed is null limit  ${LIMIT}`,
-  );
-  try {
-    Promise.all(
-      query.rows.map((document) =>
-        couchDb.insert({
-          _id: document.id.toString(),
-          ...document,
-          embed: JSON.parse(document.embed),
-        }),
+  const rows = query.filter((x) => x.embed && x.embed.length > 0);
+  if (query.length > 10) {
+    const couchx = Promise.all(
+      rows.map((document) =>
+        couchDb
+          .insert({
+            _id:
+              document.genid && document.genid.length > 2
+                ? document.genid
+                : document.id.toString(),
+            ...document,
+            enbed: JSON.parse(document.embed),
+            type: couchname,
+          })
+          .catch((e) => null),
       ),
     );
-  } catch (e) {}
 
-  const milvus = clientx.insert({
-    collection_name: COUCHDB_DB_NAME,
-    data: query.rows.map((x) => ({
-      genid: x.genid,
-      id: x.id,
-      embed: JSON.parse(x.embed),
-    })),
-  });
-
-  const updates = Promise.all(
-    query.rows.map((row) =>
-      pgClient.query(
-        `UPDATE ${TABLE} SET "processed"='1' WHERE "id" = ${row.id}`,
+    const vecs = client.upsert(couchname, {
+      wait: true,
+      points: rows.map((x) => ({
+        id: x.id,
+        payload: { ...x, type: couchname },
+        vector: JSON.parse(x.embed),
+      })),
+    });
+    const updates = Promise.all(
+      query.map(
+        (row) => sql`UPDATE qa.tags SET "processed"='1' WHERE "id" = ${row.id}`,
       ),
-    ),
-  );
-  await Promise.all([milvus, updates]);
-  console.log(query.rows[0].id, LIMIT, query.rows.length);
-  if (query.rows.length === LIMIT) {
-    runMigration();
+    );
+
+    await Promise.all([vecs, couchx, updates]);
+    console.log(query[0].id, couchname, query.length);
+    runMigration(table, couchname);
   } else {
+    console.log("DONE");
     throw new Error();
   }
 };
-
-runMigration();
+//
+runMigration(`qa.tags`, "tags");
